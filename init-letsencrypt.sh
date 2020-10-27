197f783ddb08aec7c729505095b0fcec8c5e97ad
RozennGZ <rozie229@hotmail.com>
2020-10-27 13:59:12 +0100

    modif ardata credentials

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(shiny.ardata.fr)
+domains=(app.nutrition-quantitative.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="rozenn.gazan@ms-nutrition.com" # L'ajout d'une adresse valide est fortement recommandé
